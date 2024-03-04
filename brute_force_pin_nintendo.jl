df7c9b976faaec9f8e0b44adc4c2f7d16b06eb6c
hurtbadly <divita2004@gmail.com>
2024-03-04 23:05:48 +0530

    added new changes

--- brute_force_pin_nintendo.jl
+++ brute_force_pin_nintendo.jl
@@ -4,18 +4,43 @@ tested_codes = [9239, 1084, 2154, 3185, 6127, 9909, 2786, 1111, 0000, 1234, 7286
 function try_new_code(code)
     if code in tested_codes
         println("Try another code, dont waste this try, u tried this already")
+        return false
     else
         println("Try this code , u havent tried this code yet.")
-        add_code_to_list(code)
+        return true
   end
 end
 
 
-function add_code_to_list(code)
-    push!(tested_codes,code)
+function add_code_to_list(code_array)
+    append!(tested_codes,code_array)
     println(tested_codes)
 end
 
+number_buffer = []
+
+number_of_times = 6
+while number_of_times >= 1 
+global number_of_times
 user_code = readline()
+if length(user_code) != 4
+    println("you havent entered a Correct 4 DIGIT PIN, redo")
+    user_code = readline()
+end
 user_code_numeric = parse(Int64,user_code)
-try_new_code(user_code_numeric)
+value = try_new_code(user_code_numeric)
+if value
+    push!(number_buffer,user_code_numeric)
+    number_of_times -= 1
+end
+end
+
+add_code_to_list(number_buffer)
+
+
+function remove_duplicates(number_array)
+  return [Set(number_array)...]
+end
+
+final_tested_codes_with_duplicates_removed = remove_duplicates(tested_codes)
+println(final_tested_codes_with_duplicates_removed)
